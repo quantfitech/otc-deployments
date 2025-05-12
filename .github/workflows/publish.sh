4be081c9d9e68722c833f388bc626ab4ff37883b
Lorenzo Giuliani <lorenzo@quantfi.tech>
2025-05-12 12:44:42 +0200

    feat(gha): add helm login

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env nix develop --command fish 
 # vim: ft=fish
+set -e
 
+#
+# fallback for required variables
+#
 if ! set -q $GITHUB_RUN_NUMBER
   set GITHUB_RUN_NUMBER "0"
 end
 if ! set -q $GITHUB_STEP_SUMMARY 
   set GITHUB_STEP_SUMMARY /dev/stdout
 end
+
+# generate new version id
 
 set ver "$(date +'%g.%m.')$GITHUB_RUN_NUMBER"
 set charts (fd Chart.yaml -x echo \{//\})
@@ -18,10 +24,11 @@ for i in (fd Chart.yaml)
   yq -i -y ".appVersion = \"$ver\"" $i
 end
 
+# login in the OCI repository
+helm registry login -u {$OCI_USERNAME} -p {$OCI_PASSWORD} {$REGISTRY}
+
 set packages
 for i in $charts
-  set i (string replace './' '' $i)
-
   helm package $i
   helm publish $i-* {$REGISTRY}/{$IMAGE_NAME}
 
@@ -31,10 +38,15 @@ end
 echo packages: $packages
 echo summaryfile: $GITHUB_STEP_SUMMARY
 
-echo "# Built packages
+echo "# Built helm charts:
 " >> $GITHUB_STEP_SUMMARY
 for pack in $packages
-  echo "- $pack, $(echo $pack-*)"
+  echo "- $pack, **$(echo $pack-*)**"
 end >>$GITHUB_STEP_SUMMARY
 
-echo "registry: {$REGISTRY}/{$IMAGE_NAME}"
+echo "
++---------------+---------------------------+
+| registry      | {$REGISTRY}/{$IMAGE_NAME} |
+| chart version | {$ver}                    |
++---------------+---------------------------+
+"

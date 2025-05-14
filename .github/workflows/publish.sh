8b0b9a8bf3245246200843c2ba65f8dc6dd5e0cf
Lorenzo Giuliani <lorenzo@quantfi.tech>
2025-05-14 19:50:28 +0200

    feat(gha): publish

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env nix develop --command fish 
 # vim: ft=fish
-set -e
 
-#
+# check hard requirements
+# NOTE: `begin; [cmd...]; end` blocks evaluate commands returning the first failure or the last success
+if ! begin; set -q $OCI_USERNAME; set -q $OCI_PASSWORD; set -q $REGISTRY; set -q $IMAGE_NAME; end
+  echo "missing required environment variables:"
+  echo "- \$OCI_USERNAME"
+  echo "- \$OCI_PASSWORD"
+  echo "- \$REGISTRY"
+  echo "- \$IMAGE_NAME"
+  exit 1
+end
+
 # fallback for required variables
-#
 if ! set -q $GITHUB_RUN_NUMBER
+  echo "WARN: required variable \$GITHUB_RUN_NUMBER is not present. falling back to 0"
   set GITHUB_RUN_NUMBER "0"
 end
 if ! set -q $GITHUB_STEP_SUMMARY 
+  echo "WARN: required variable \$GITHUB_STEP_SUMMARY is not present. falling back to '/dev/stdout'"
   set GITHUB_STEP_SUMMARY /dev/stdout
 end
 
@@ -17,7 +27,7 @@ end
 set ver "$(date +'%g.%m.')$GITHUB_RUN_NUMBER"
 set charts (fd Chart.yaml -x echo \{//\})
 
-echo generated version: $ver
+echo "generated version: $ver"
 
 for i in (fd Chart.yaml)
   yq -i -y ".version = \"$ver\"" $i
@@ -25,28 +35,43 @@ for i in (fd Chart.yaml)
 end
 
 # login in the OCI repository
-helm registry login -u {$OCI_USERNAME} -p {$OCI_PASSWORD} {$REGISTRY}
+helm registry login \
+  -u {$OCI_USERNAME} \
+  -p {$OCI_PASSWORD} \
+  {$REGISTRY}
 
 set packages
 for i in $charts
   helm package $i
-  helm publish $i-* {$REGISTRY}/{$IMAGE_NAME}
+  helm push $i-* {$REGISTRY}/{$IMAGE_NAME}
 
   set packages $packages $i
 end
 
-echo packages: $packages
-echo summaryfile: $GITHUB_STEP_SUMMARY
+#
+# Git PR
+#
+git config set "email" "${GITHUB_ACTOR}@users.noreply.github.com"
+git config set "name" "github-actions"
 
-echo "# Built helm charts:
-" >> $GITHUB_STEP_SUMMARY
-for pack in $packages
-  echo "- $pack, **$(echo $pack-*)**"
-end >>$GITHUB_STEP_SUMMARY
 
-echo "
-+---------------+---------------------------+
-| registry      | {$REGISTRY}/{$IMAGE_NAME} |
-| chart version | {$ver}                    |
-+---------------+---------------------------+
+#
+# Summary
+#
+begin
+  echo "packages: $packages"
+  echo "summaryfile: $GITHUB_STEP_SUMMARY"
+
+  echo "# Built helm charts:"
+
+  for pack in $packages
+    echo "- $pack, **$(echo $pack-*)**"
+  end
+
+  echo "
++---------------+-----------------------+
+| registry      | $REGISTRY/$IMAGE_NAME |
+| chart version | $ver                  |
++---------------+-----------------------+
 "
+end  >> $GITHUB_STEP_SUMMARY
